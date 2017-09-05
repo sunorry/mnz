bd19fae57910d0cc0ed57188885f5afd1096b9b7
sunorry <sunorry@gmail.com>
2017-09-05 18:06:03 +0800

    ADD config.js for mnz_scaffold

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'exports': [
+        './src/main.js',
+        './src/css/base.scss'
+    ],
+    'alias': {
+        '@': 'src'
+    },
+    sourcePath: '//fe.benmu-health.com/mnz/dist'
+};
